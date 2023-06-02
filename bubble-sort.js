b9d48a024581888f879db3e06bd6045e686d7b67
arunOpt <arun@optisolbusiness.com>
2023-06-02 15:13:48 +0530

    cleanup

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,32 +1,36 @@
 //To sort array in asc order using bubble sort algorithm
 
 //Input
-const arrayToSort=[]
+const arrayToSort =[8,6,1, 7, 2,4]
 //code
-function bubbleSort(arrayToSort){
-    let result=[...arrayToSort]
-    let swap=true
-    const length=arrayToSort.length;
 
-    while(swap===true){
-        let iterations=0
-        for (let i=0; i<length-1; i++){
-            if(result[i]>result[i+1]){
+function bubbleSort(arrayToSort) {
+    let result = [...arrayToSort]
+    let swap = true
+    const length = arrayToSort.length;
+    let loop = 0
+    let totalLoop = 0
+    while (swap === true) {
+        loop++
+        let iterations = 0
+        for (let i = 0; i < length - 1; i++) {
+            totalLoop++
+            if (result[i] > result[i + 1]) {
                 iterations++
-                let temp=  result[i]
-                result[i]=result[i+1];
-                result[i+1]=temp           
+                let temp = result[i]
+                result[i] = result[i + 1];
+                result[i + 1] = temp
             }
         }
-        if(iterations===0){
-            swap=false
+        if (iterations === 0) {
+            swap = false
         }
     }
-
-return result
+    console.log(loop, totalLoop);
+    return result
 }
-const result= bubbleSort(arrayToSort)
-console.log(result)
 
 //Output
+const result = bubbleSort(arrayToSort)
+console.log(result)
 // [1, 2, 4, 6, 7, 8]
